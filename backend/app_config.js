3a6d70c6ba3add6e427aa5a7ebc665bbd4f9947b
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2016-10-10 10:34:05 -0300

    Update app_config.js

--- backend/app_config.js
+++ backend/app_config.js
@@ -7,9 +7,9 @@ var bodyParser = require('body-parser');
 
 var allowCors = function(req, res, next) {
 
-	 res.setheader('Access-Control-Allow-Origin', '*');
+	// res.setheader('Access-Control-Allow-Origin', '*');
     //  res.setheader('Access-Control-Allow-Origin', 'GET,POST,PUT,DELETE');
-       res.setheader('Access-Control-Allow-Origin', 'X-Requested-With, content-type, Authorization');
+     //  res.setheader('Access-Control-Allow-Origin', 'X-Requested-With, content-type, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
